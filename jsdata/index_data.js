71e9b2029d9096e9fd7bbd748d94e205b668377f
wise-man-1 <2810201146@qq.com>
2018-10-04 20:07:24 +0800

    1
    1

---
+++ jsdata/index_data.js
@@ -0,0 +1,14 @@
+// window.onload=function(){
+//     //首页初始化
+//     index_Initialize()
+// }
+
+
+// function index_Initialize(){
+//     t1 = "手工创意五角星"
+//     t2 = "自由拍摄"
+//     t3 = "空闲时光"
+//     $('#shuffling-text1').html('NEW <strong></strong>'+t1)
+//     $('#shuffling-text2').html(t2)
+//     $('#shuffling-text3').html(t3)
+// }
