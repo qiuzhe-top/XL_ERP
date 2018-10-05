5bb0b6b66005fba4ebe458122146fd7bb8de1ed1
wise-man-1 <2810201146@qq.com>
2018-10-05 08:48:20 +0800

    1
    1

--- jsdata/mp4_data.js
+++ jsdata/mp4_data.js
@@ -3,34 +3,34 @@ var MP4list =new Array;
 window.onload=function(){
     //数据初始化
     //》》》 JPG_DATA(1,2,3,4,5) 《《《    数据格式  图片地址 / 标题 / 标签（制作人） / 简介 / 跳转地址  》》 复制这段数据即可
-    JPG_DATA('http://www.huimengya.com/uploads/allimg/160810/1-160Q02210530-L.jpg','1','作者：佳慧发布时间：2016-08-13','转手绘（又名：转鼠绘，转手绘,仿鼠绘）是利用图片处理工具，将人物或风景照片转变成类似手绘效果的一种图片处理转手绘（又名：转鼠绘，转手绘,仿鼠绘）是利用图片处理工具，将人物或风景照片转变成类似手绘效果的一种图片处理方法。全名，照片转手绘效果转手绘（又名：转鼠绘，转手绘,仿鼠绘）是利用图片处理工具，将人物或风景照片转变成类似手绘效果的一种图片处理方法。全名，照片转手绘效果转手绘（又名：转鼠绘，转手绘,仿鼠绘）是利用图片处理工具，将人物或风景照片转变成类似手绘效果的一种图片处理方法。全名，照片转手绘效果转手绘（又名：转鼠绘，转手绘,仿鼠绘）是利用图片处理工具，将人物或风景照片转变成类似手绘效果的一种图片处理方法。全名，照片转手绘效果方法。全名，照片转手绘效果','http://www.huimengya.com/p/406.html')
-    JPG_DATA('http://www.huimengya.com/uploads/allimg/160810/1-160Q02210530-L.jpg','2','作者：佳慧发布时间：2016-08-13','转手绘（又名：转鼠绘，转手绘,仿鼠绘）是利用图片处理工具，将人物或风景照片转变成类似手绘效果的一种图片处理方法。全名，照片转手绘效果','http://www.huimengya.com/p/406.html')
+    MP4_DATA('//player.bilibili.com/player.html?aid=23920182&cid=40020512&page=1','谁的青春不迷茫','团队：小王 发布时间：2016-08-13','运用所学PR加AE制作','http://www.huimengya.com/p/406.html')
     index_Initialize()
+    // alert(2)
 }
-function JPG_DATA(t1,t2,t3,t4,t5){
-    var JPG_DIC = new Object;
-    var JPG_DIC ={
+function MP4_DATA(t1,t2,t3,t4,t5){
+    var MP4_DATA = new Object;
+    var MP4_DATA ={
         img_url : t1,
         title  :  t2,
         name   :  t3,
         describe :t4,
         jump_url :t5
     }
-    MP4list.push(JPG_DIC)
+    MP4list.push(MP4_DATA)
 }
 function index_Initialize(){
 // $('.row-bottom-padded-md').html('')
 for(i in MP4list){
-    t1 = '<div class="col-md-3 col-sm-6 col-padding animate-box" data-animate-effect="fadeInLeft" style="opacity: 1;">'
-    t2 =   ' <div class="blog-entry">'
-    t3 =       ' <a href="#" class="blog-img"><img src="' + MP4list[i].img_url + '" class="img-responsive" alt="Free HTML5 Bootstrap Template by FreeHTML5.co"></a>'
-    t4 =      ' <div class="desc">'
-    t5 =           '<h3><a href="#">' +MP4list[i].title + '</a></h3>'
-    t6 =          ' <span>' +MP4list[i].name + '</span>'
-    t7 =           '<p>' +MP4list[i].describe + '</p>'
-    t8 =           '<a href="' +MP4list[i].jump_url + '" class="lead" target="_blank">Read More <i class="icon-arrow-right3"></i></a>'
-    t9 =       '</div>'
-    t10 =    '</div>'
+    t1 = '<div class="col-md-4 col-sm-6 col-padding animate-box" data-animate-effect="fadeInLeft" style="opacity: 1;"> '
+    t2 = '<div class="blog-entry"> '
+    t3 =    '<a href="#" class="blog-img">' 
+    t4 = '<iframe src="' + MP4list[i].img_url + '" scrolling="no" border="0" frameborder="no" framespacing="0" allowfullscreen="true" style="box-shadow: 0 0 1px 1px rgb(199, 199, 199); width:100%;height: 250px;"> </iframe>'
+    t5 =    '</a> '
+    t6 =    '<div class="desc"><h3>'
+    t7 =        '<a href="#">' +MP4list[i].title +'</a></h3> <span>' +MP4list[i].name + '</span>'
+    t8 =        '<p>' +MP4list[i].describe + '</p>'
+    t9 =        '<a href="' +MP4list[i].jump_url +'" class="lead" target="_blank">Read More <i class="icon-arrow-right3"></i></a></div>'
+    t10 = '</div>'
     t11 = '</div>'
     data = t1+t2+t3+t4+t5+t6+t7+t8+t9+t10+t11
     $('.row-bottom-padded-md').append(data)
